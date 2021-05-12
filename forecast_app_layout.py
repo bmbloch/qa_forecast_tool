894dbde881221f46defd25e10cf4b08076d450e2
Ben <ben.bloch@moodys.com>
2021-05-12 16:56:43 -0400

    Adding div to hold the confirm dialogue box for finalizer button click

--- forecast_app_layout.py
+++ forecast_app_layout.py
@@ -53,6 +53,13 @@ def get_app_layout():
                         )
                     ], style={'text-align': 'center', 'vertical-align': 'middle'}),
                     html.Div([
+                        dcc.ConfirmDialog(
+                        id='confirm_finalizer',
+                        displayed=False,
+                        message="Clicking OK will finalize the forecast and overwrite any existing finalized files previously created for this month"
+                        ),
+                    ]),
+                    html.Div([
                         dbc.Alert(
                             html.P(id='logic_alert_text'),
                             id = "finalizer_logic_alert",

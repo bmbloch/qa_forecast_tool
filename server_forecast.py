71b96698bb4e6cd02244bd03cf5a916189aedd31
Ben <ben.bloch@moodys.com>
2021-07-06 09:25:58 -0400

    Changing browser tab name from defualt of Dash to Forecast Review

--- server_forecast.py
+++ server_forecast.py
@@ -1,10 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
 
-forecast = dash.Dash(
-    __name__,
-    external_stylesheets=[dbc.themes.BOOTSTRAP]
-)
+forecast = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], title='Forecast Review')
 
 forecast.config.suppress_callback_exceptions = True
 
